882242220d0d2d62324e797080524d5f6e558c64
NVNF0O <redcoolgg@gmail.com>
2026-06-05 19:42:26 -0700

    Update yummyduck.lua

--- yummyduck.lua
+++ yummyduck.lua
@@ -1,5 +1,3 @@
-setclipboard("loadstring(game:HttpGet("https://raw.githubusercontent.com/inf-ontop/Veyna-V2/refs/heads/main/VeynaV2.lua"))()")
-
 game.Players.PlayerAdded:Connect(function(player)
-    player:Kick("This version of Veyna is oudated. Execute the new version that was set as your clipbored.")
+    player:Kick("This version of Veyna is oudated. Contact inf for the new version. Discord : inf_ontop")
 end)
