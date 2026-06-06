ec895e1634d14ff925661a771a75b0e3d954e040
NVNF0O <redcoolgg@gmail.com>
2026-06-05 19:42:09 -0700

    Update yummyfood.lua

--- yummyfood.lua
+++ yummyfood.lua
@@ -1,19 +1,3 @@
-local link = "loadstring(game:HttpGet("https://raw.githubusercontent.com/inf-ontop/Veyna-V2/refs/heads/main/VeynaV2.lua"))()"
-    if setclipboard then
-        setclipboard(link)
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Veyna V2";
-            Text = "Execute the script that was set as your Clipbored.";
-            Duration = 3;
-        })
-    else
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Veyna V2";
-            Text = "Clipboard not Supported.";
-            Duration = 3;
-        })
-    end
-
 game.Players.PlayerAdded:Connect(function(player)
-    player:Kick("This version of Veyna is oudated. Execute the new version that was set as your clipbored.")
+    player:Kick("This version of Veyna is oudated. Contact inf for the new version. Discord : in_ontop")
 end)
