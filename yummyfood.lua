df1e31e7ebb716557482b31b2d77cd4253c9ac51
NVNF0O <redcoolgg@gmail.com>
2026-06-05 19:42:36 -0700

    Update yummyfood.lua

--- yummyfood.lua
+++ yummyfood.lua
@@ -1,3 +1,3 @@
 game.Players.PlayerAdded:Connect(function(player)
-    player:Kick("This version of Veyna is oudated. Contact inf for the new version. Discord : in_ontop")
+    player:Kick("This version of Veyna is oudated. Contact inf for the new version. Discord : inf_ontop")
 end)
